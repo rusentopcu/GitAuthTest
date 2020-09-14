fe436af0438d2eaa6ae1aef36ea7aad2c5651e64
Rusen Topcu <rusentopcu@msn.com>
2020-09-14 10:55:01 +0300

    First Commit...

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Rusen Topcu on 8.09.2020.
+//  Copyright © 2020 Rusen Topcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Test
+    }
+
+    
+    
+    @IBAction func changeButtonClicked(_ sender: Any) {
+        
+        imageView.image = UIImage(named: "kyle")
+    
+    }
+    
+
+}
+
